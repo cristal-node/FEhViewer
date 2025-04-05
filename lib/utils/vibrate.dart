97b103c13011e93ca9ab73184c8add549c68772f
cristal-node <47137955+cristal-node@users.noreply.github.com>
2025-04-05 04:56:21 +0000

    "flutter_vibrate" no longer needed

--- lib/utils/vibrate.dart
+++ lib/utils/vibrate.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 import 'package:eros_fe/common/service/ehsetting_service.dart';
-import 'package:flutter_vibrate/flutter_vibrate.dart';
 import 'package:get/get.dart';
 import 'package:vibration/vibration.dart';
+import 'package:vibration/vibration_presets.dart';
 
 import 'logger.dart';
 
@@ -12,85 +12,57 @@ final VibrateUtil vibrateUtil = VibrateUtil();
 class VibrateUtil {
   VibrateUtil();
 
-  /// 设备是否具有控制振动幅度的能力
-  Future<bool?> get _hasCustomVibrationsSupport =>
-      Vibration.hasCustomVibrationsSupport();
-
-  /// 设备是否能够按照自定义的持续时间，模式或强度振动
-  Future<bool?> get _hasAmplitudeControl => Vibration.hasAmplitudeControl();
-
   EhSettingService get _ehSettingService => Get.find();
 
   Future<void> impact() async {
-    if (!_ehSettingService.vibrate.value) {
-      return;
-    }
-    if (Platform.isIOS || (await _hasAmplitudeControl ?? false)) {
-      Vibrate.feedback(FeedbackType.impact);
-    } else if (Platform.isAndroid) {
-      Vibration.vibrate(duration: 2);
+    if (_ehSettingService.vibrate.value || await Vibration.hasVibrator()) {
+      if (await Vibration.hasAmplitudeControl()) {
+        Vibration.vibrate(duration: 2);
+      } else {
+        Vibration.vibrate(preset: VibrationPreset.softPulse);
+      }
     }
   }
 
   Future<void> light() async {
-    if (!_ehSettingService.vibrate.value) {
-      return;
-    }
-    if (Platform.isIOS) {
-      Vibrate.feedback(FeedbackType.light);
-    } else if (Platform.isAndroid && (await _hasAmplitudeControl ?? false)) {
-      Vibration.vibrate(amplitude: 100, duration: 5);
-    } else if (Platform.isAndroid) {
-      Vibration.vibrate(duration: 5);
+    if (_ehSettingService.vibrate.value || await Vibration.hasVibrator()) {
+      if (await Vibration.hasAmplitudeControl()) {
+        Vibration.vibrate(amplitude:100, duration: 5);
+      } else {
+        Vibration.vibrate(preset: VibrationPreset.softPulse);
+      }
     }
   }
 
   Future<void> medium() async {
-    logger
-        .v('_hasCustomVibrationsSupport:${await _hasCustomVibrationsSupport}\n'
-            '_hasAmplitudeControl:${await _hasAmplitudeControl}');
-
-    if (!_ehSettingService.vibrate.value) {
-      return;
-    }
-
-    if (Platform.isIOS) {
-      Vibrate.feedback(FeedbackType.medium);
-    } else if (Platform.isAndroid && (await _hasAmplitudeControl ?? false)) {
-      Vibration.vibrate(amplitude: 120, duration: 15);
-    } else if (Platform.isAndroid) {
-      Vibration.vibrate(duration: 20);
+    if (_ehSettingService.vibrate.value || await Vibration.hasVibrator()) {
+      if (await Vibration.hasAmplitudeControl()) {
+        Vibration.vibrate(amplitude:120, duration: 15);
+      } else {
+        Vibration.vibrate(preset: VibrationPreset.softPulse);
+      }
     }
   }
 
   Future<void> heavy() async {
-    if (!_ehSettingService.vibrate.value) {
-      return;
-    }
-    if (Platform.isIOS) {
-      Vibrate.feedback(FeedbackType.heavy);
-    } else if (Platform.isAndroid && (await _hasAmplitudeControl ?? false)) {
-      Vibration.vibrate(amplitude: 255, duration: 10);
-    } else if (Platform.isAndroid) {
-      Vibration.vibrate(duration: 35);
+    if (_ehSettingService.vibrate.value || await Vibration.hasVibrator()) {
+      if (await Vibration.hasAmplitudeControl()) {
+        Vibration.vibrate(amplitude:255, duration: 10);
+      } else {
+        Vibration.vibrate(preset: VibrationPreset.softPulse);
+      }
     }
   }
 
   Future<void> success() async {
-    if (!_ehSettingService.vibrate.value) {
-      return;
-    }
-    if (Platform.isIOS) {
-      Vibrate.feedback(FeedbackType.success);
+    if (_ehSettingService.vibrate.value || await Vibration.hasVibrator()) {
+      Vibration.vibrate(preset: VibrationPreset.softPulse);
     }
   }
 
   Future<void> error() async {
-    if (!_ehSettingService.vibrate.value) {
-      return;
-    }
-    if (Platform.isIOS) {
-      Vibrate.feedback(FeedbackType.error);
+    if (_ehSettingService.vibrate.value || await Vibration.hasVibrator()) {
+      Vibration.vibrate(preset: VibrationPreset.heartbeatVibration);
     }
   }
 }
